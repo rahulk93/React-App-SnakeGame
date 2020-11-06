6056e92c4384abb7e3a85079ad861c571707d3a2
Rahul Khatri <forever_rahul16@hotmail.com>
2020-11-06 16:09:05 +1300

    Updates

---
+++ precache-manifest.4470d4b65cdb32d3a0b05c5fd7d669e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61694b866ff54b3668646c4274e6f5e",
+    "url": "/React-App-SnakeGame/index.html"
+  },
+  {
+    "revision": "3d558484c22225005649",
+    "url": "/React-App-SnakeGame/static/css/main.d92112d7.chunk.css"
+  },
+  {
+    "revision": "c91a4dda49eadd49b534",
+    "url": "/React-App-SnakeGame/static/js/2.9d01f88a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-SnakeGame/static/js/2.9d01f88a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d558484c22225005649",
+    "url": "/React-App-SnakeGame/static/js/main.c04a1e93.chunk.js"
+  },
+  {
+    "revision": "2d2d2952099759e93509",
+    "url": "/React-App-SnakeGame/static/js/runtime-main.710a64ef.js"
+  }
+]);
